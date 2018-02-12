32c27c6a738d4ce02af4149a901a996674f8dfbf
stevenjburns@msn.com <stevenjburns@msn.com>
2018-02-12 07:57:21 -0700

    Moving original files from OneDrive to this repo

---
+++ MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SilverlightTest003
+	{
+	public partial class MainPage : UserControl
+		{
+		public MainPage()
+			{
+			InitializeComponent();
+			}
+		}
+	}
